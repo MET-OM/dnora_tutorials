50ef4aa9db4bbf94bbb04305d6688fca74bbc724
Konstantinos Christakos <67804784+KonstantinChri@users.noreply.github.com>
2025-09-13 14:28:42 +0200

    Create ex1_swan_nostat.py

---
+++ tutorials/ex1_swan_nostat.py
@@ -0,0 +1,44 @@
+import dnora as dn
+
+# User definitions for run
+NAME = "Ex1_Sula500"
+START_TIME = "2020-01-20T00:00"
+END_TIME = "2020-01-20T23:00"
+DM = 500  # Grid resolution in meters
+LON = (5.21, 6.66)
+LAT = (62.25, 62.89)
+BOUNDARY_EDGES = ["N", "W"]
+SPECTRAL_OUTPUT_LON = [5.89, 5.46]
+SPECTRAL_OUTPUT_LAT = [62.48, 62.75]
+
+
+def main() -> None:
+    # Define the area and set the grid resolution and bathymetry
+    grid = dn.grid.EMODNET(lon=LON, lat=LAT, name=NAME)
+    grid.import_topo()
+    grid.set_spacing(dm=DM)
+    grid.mesh_grid()
+    grid.set_boundary_points(dn.grid.mask.Edges(edges=BOUNDARY_EDGES))
+    grid.set_output_points(
+        dn.grid.mask.LonLat(lon=SPECTRAL_OUTPUT_LON, lat=SPECTRAL_OUTPUT_LAT)
+    )
+    # Define a ModelRun for the desired area
+    model = dn.modelrun.NORA3(grid, start_time=START_TIME, end_time=END_TIME)
+    model.import_wind()
+    model.import_spectra()
+    model.plot.grid()
+
+    # Write grid file for SWAN
+    exporter = dn.export.SWAN(model)
+    exporter.export_grid()
+    exporter.export_wind()
+    exporter.export_spectra()
+
+    # Write input file for model and execute SWAN
+    exe = dn.executer.SWAN(model)
+    exe.write_input_file()
+    exe.run_model()
+
+
+if __name__ == "__main__":
+    main()
